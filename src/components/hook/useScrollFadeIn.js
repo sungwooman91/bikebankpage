4b08bc97538da0bbdaa71eac181b3aa2baeabf50
SUNG WOO YOON <tjddn636@naver.com>
2022-08-02 17:32:22 +0900

    Create useScrollFadeIn.js
    [22.08.22] 스크롤 애니메이션 구현. 커스텀하게는 사용 못함

---
+++ src/components/hook/useScrollFadeIn.js
@@ -0,0 +1,40 @@
+// @flow
+import { useCallback, useEffect, useRef } from "react";
+
+export const useScrollFadeIn = () => {
+  const dom = useRef();
+
+  const handleScroll = useCallback(([entry]) => {
+    const { current } = dom;
+
+    if (entry.isIntersecting) {
+      // 원하는 이벤트를 추가 할 것
+      current.style.transitionProperty = "opacity transform";
+      current.style.transitionDuration = "1s";
+      current.style.transitionTimingFunction = "cubic-bezier(0, 0, 0.2, 1)";
+      current.style.transitionDelay = "0s";
+      current.style.opacity = 1;
+      current.style.transform = "translate3d(0, 0, 0)";
+    }
+  }, []);
+
+  useEffect(() => {
+    let observer;
+    const { current } = dom;
+
+    if (current) {
+      observer = new IntersectionObserver(handleScroll, { threshold: 0.7 });
+      observer.observe(current);
+
+      return () => observer && observer.disconnect();
+    }
+  }, [handleScroll]);
+
+  return {
+    ref: dom,
+    style: {
+      opacity: 0,
+      transform: "translate3d(0, 50%, 0)",
+    },
+  };
+};
